6f13fe0764f2d869adf4009d92f7510b2bf4b52a
Bkc4444 <bkc4444@gmail.com>
2022-07-30 17:07:37 -0700

    Added DB context class

---
+++ eCommerceSite/eCommerceSite/Data/MusicStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Data
+{
+    public class MusicStoreContext : DbContext
+    {
+        /// <summary>
+        /// This builds a constructor for the database
+        /// </summary>
+        /// <param name="options"></param>
+        public MusicStoreContext(DbContextOptions<MusicStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Music> Musics { get; set; }
+    }
+}
