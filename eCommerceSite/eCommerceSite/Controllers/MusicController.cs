6e7f52b402dd2c77b90bdfaabf9d4998bc6b7534
Bkc4444 <bkc4444@gmail.com>
2022-08-12 16:27:40 -0700

    Create game fully functional, added some styling

--- eCommerceSite/eCommerceSite/Controllers/MusicController.cs
+++ eCommerceSite/eCommerceSite/Controllers/MusicController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
     public class MusicController : Controller
     {
+        private readonly MusicStoreContext _context;
+
+        public MusicController(MusicStoreContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Music m)
+        {
+            if (ModelState.IsValid)
+            {
+                // add to the database
+                _context.Musics.Add(m); // prepares insert
+                _context.SaveChanges();// Executes pending insert
+
+                //show success message on page
+                ViewData["Message"] = $"{m.Title} was added successfully!";
+                return View();
+            }
+
+            return View(m);
         }
     }
 }
