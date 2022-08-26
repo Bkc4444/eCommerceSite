e23859a65393771116854bca4c8f8d7e067300a9
Brandon Carroll <bkc4444@gmail.com>
2022-08-25 23:24:45 -0700

    Added migration for Member class

--- eCommerceSite/eCommerceSite/Migrations/20220826061641_AddedMember.Designer.cs
+++ eCommerceSite/eCommerceSite/Migrations/20220826061641_AddedMember.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceSite.Data;
 
@@ -10,9 +11,10 @@ using eCommerceSite.Data;
 namespace eCommerceSite.Migrations
 {
     [DbContext(typeof(MusicStoreContext))]
-    partial class MusicStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220826061641_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
