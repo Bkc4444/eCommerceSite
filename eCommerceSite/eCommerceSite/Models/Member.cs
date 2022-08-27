1b0139f5bb9274bf599ee0671f7d33ead2def6c0
Brandon Carroll <bkc4444@gmail.com>
2022-08-26 19:11:05 -0700

    Added Register ViewModel

--- eCommerceSite/eCommerceSite/Models/Member.cs
+++ eCommerceSite/eCommerceSite/Models/Member.cs
@@ -15,4 +15,26 @@ namespace eCommerceSite.Models
 
         public string Username { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]//makes this look like an email address input
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]//using nameof so that we can use the class name instead of a string
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConformPassword { get; set; }
+    }
 }
